84917f4e0c46b6c284c1b58ad1dd8a08011cc7a5
sseen2 <kse10023@naver.com>
2025-11-01 17:07:16 +0900

    test: 도메인 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +22,24 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 개수가 6개보다 적은 경우 예외 발생")
+    @Test
+    void lottoNumberTooFew() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1보다 작은 경우 예외 발생")
+    @Test
+    void lottoNumberTooSmall() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 45보다 큰 경우 예외 발생")
+    @Test
+    void lottoNumberTooBig() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
---
+++ src/test/java/lotto/WinningNumberTest.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumberTest {
+
+    private Lotto winningNumber;
+
+    @BeforeEach
+    void setUp() {
+        winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 같은 경우 예외 발생")
+    @Test
+    void winningNumberEqualBonusNumber() {
+        int bonusNumber = 6;
+
+        assertThatThrownBy(() -> new WinningNumber(winningNumber, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1보다 작은 경우 예외 발생")
+    @Test
+    void bonusNumberTooSmall() {
+        int bonusNumber = 0;
+
+        assertThatThrownBy(() -> new WinningNumber(winningNumber, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 45보다 큰 경우 예외 발생")
+    @Test
+    void bonusNumberTooBig() {
+        int bonusNumber = 46;
+
+        assertThatThrownBy(() -> new WinningNumber(winningNumber, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
