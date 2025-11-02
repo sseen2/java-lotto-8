23377e30d6444de9fa151c9df381cfce1a539970
sseen2 <kse10023@naver.com>
2025-11-02 14:14:04 +0900

    feat: 일치 개수 수정 및 미당첨 상수 추가

---
+++ src/main/java/lotto/domain/LottoRanking.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public enum LottoRanking {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean matchBonusNumber;
+    private final int prize;
+
+    LottoRanking(int matchCount, boolean matchBonusNumber, int prize) {
+        this.matchCount = matchCount;
+        this.matchBonusNumber = matchBonusNumber;
+        this.prize = prize;
+    }
+}
