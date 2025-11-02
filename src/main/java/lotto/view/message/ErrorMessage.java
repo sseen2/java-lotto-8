846a8b697b2a0ee9e4d68d938643cd0248965807
sseen2 <kse10023@naver.com>
2025-11-02 13:54:53 +0900

    feat: 에러 메세지 내용 수정

--- src/main/java/lotto/view/message/ErrorMessage.java
+++ src/main/java/lotto/view/message/ErrorMessage.java
@@ -4,9 +4,8 @@ public enum ErrorMessage {
 
     ERROR_MESSAGE("[ERROR] "),
     PURCHASE_AMOUNT_INVALID_FORMAT("로또 금액은 숫자여야 합니다."),
-    PURCHASE_AMOUNT_NOT_ZERO("로또 금액은 0 이상이어야 합니다."),
-    PURCHASE_AMOUNT_NOT_NEGATIVE("로또 금액은 양수여야 합니다."),
-    PURCHASE_AMOUNT_NOT_DIVIDE_UP("로또 금액은 "),
+    PURCHASE_AMOUNT_INVALID_AMOUNT("로또 금액은 %d원 이상이어야 합니다."),
+    PURCHASE_AMOUNT_NOT_DIVIDE_UP("로또 금액은 %d원으로 나누어 떨어저야 합니다."),
     WINNING_NUMBER_INVALID_FORMAT("당첨 번호는 숫자여야 합니다."),
     WINNING_NUMBER_INVALID_COUNT("당첨 번호의 개수는 6개여야 합니다."),
     WINNING_NUMBER_OUT_OF_RANGE("당첨 번호는 1에서 45 사이 숫자여야 합니다."),
