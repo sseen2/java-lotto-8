24a74e4f04e7e3024383d22b7e57a827f72fa915
sseen2 <kse10023@naver.com>
2025-11-03 22:42:53 +0900

    style: 불필요한 import문 정리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,6 @@ package lotto.view;
 
 import static lotto.domain.LottoConstants.LOTTO_PRIZE;
 
-import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.Ranking;
