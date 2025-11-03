dfb69039974f8c200ec3e3830796e1f2ebe6dc26
sseen2 <kse10023@naver.com>
2025-11-03 19:33:02 +0900

    feat: 로또 2등 당첨 문구 추가

--- src/main/java/lotto/view/message/OutputMessage.java
+++ src/main/java/lotto/view/message/OutputMessage.java
@@ -5,6 +5,7 @@ public enum OutputMessage {
     PURCHASE_COUNT("%d개를 구매했습니다."),
     WINNING_INFORMATION("당첨 통계\n---"),
     WINNING_INFORMATION_FORMAT("%d개 일치 (%,d원) - %d개"),
+    WINNING_INFORMATION_SECOND_FORMAT("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
     RATE_OF_PROFIT("총 수익률은 %.1f%%입니다.");
 
     private final String message;
